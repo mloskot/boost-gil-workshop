1761f64ab39cb175b515a62b5e58cce33cadaac0
unknown <mateusz@loskot.net>
2010-12-23 17:46:59 +0000

    Started sample test

--- test/gil_gdal_test.cpp
+++ test/gil_gdal_test.cpp
@@ -1,11 +1,17 @@
 #include <array>
 #include <cassert>
 
-//namespace bgil = boost::gil;
+#include <boost/gil/gil_all.hpp>
+#include <gil/extension/io/gdal_read.hpp>
+
+namespace bgil = boost::gil;
 
 int main()
 {
 
+    std::string filename( "image.tif" );
+    bgil::rgb8_image_t img;
+    bgil::read_image(filename, img, gdal_tag() );
 
     return 0;
 }
