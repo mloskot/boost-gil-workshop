21d0d3eed1e2a1b03c110b1598f445ff18839d3f
unknown <mateusz@loskot.net>
2011-01-06 16:15:15 +0000

    Find and link against GDAL library from OSGeo4W

--- configure.bat
+++ configure.bat
@@ -1,10 +1,17 @@
 @ECHO OFF
 REM *** YOUR CONFIGURATION ***
 set GIL_GDAL_TEST_BUILD_DIR="..\build"
-set BOOST_ROOT="G:\dev\boost\_svn\trunk"
+set BOOST_ROOT=G:\dev\boost\_svn\trunk
+set OSGEO4W_ROOT=D:\OSGeo4W
 REM ********************************
+
+set PATH=%OSGEO4W_ROOT%\apps\gdal-dev\bin;%OSGEO4W_ROOT%\bin;%PATH%
+set GDAL_DATA=%OSGEO4W_ROOT%\apps\gdal-dev\share\gdal
 
 set GIL_GDAL_TEST_DIR=%CD%
 cd %GIL_GDAL_TEST_BUILD_DIR%
-cmake -DBOOST_ROOT=%BOOST_ROOT% %GIL_GDAL_TEST_DIR%
+cmake ^
+    -DBOOST_ROOT=%BOOST_ROOT% ^
+    -DOSGEO4W_ROOT=%OSGEO4W_ROOT% ^
+    %GIL_GDAL_TEST_DIR%
 cd %GIL_GDAL_TEST_DIR%
