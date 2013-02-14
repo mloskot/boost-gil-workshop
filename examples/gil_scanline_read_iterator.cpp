be16e7df4513e8c2dd01f48fdaf01427f802bfa1
Mateusz Loskot <mateusz@loskot.net>
2013-02-14 00:40:22 +0000

    Updated scanline_read_iterator with test image and a few remarks
    Does iterator feels like std::advance specialised for reader with
    scanline as step and buffer as pointer to current location, doesn't it?

--- examples/gil_scanline_read_iterator.cpp
+++ examples/gil_scanline_read_iterator.cpp
@@ -10,7 +10,7 @@ int main()
     typedef rgb8_image_t image_t;
 
     // read image using scanline_read_iterator
-    const char* file_name = "";
+    const char* file_name = "D:\\dev\\gil\\_git\\boost-gil-workshop\\images\\g04.bmp";
 
     typedef scanline_reader
         <
@@ -18,20 +18,18 @@ int main()
         > reader_t;
 
     reader_t reader = make_scanline_reader( file_name, bmp_tag() );
-
     image_t dst( reader._info._width, reader._info._height );
     byte_t* buffer = new byte_t[ reader._scanline_length ];
-
-    scanline_read_iterator< reader_t > it  = scanline_read_iterator< reader_t >( reader, buffer );
-    scanline_read_iterator< reader_t > end = scanline_read_iterator< reader_t >();
-
+    scanline_read_iterator<reader_t> it  = scanline_read_iterator<reader_t>(reader, buffer);
+    scanline_read_iterator<reader_t> end = scanline_read_iterator<reader_t>();
     int row = 0;
     while( it != end )
     {
-        *it;
+        *it; // ?
 
         copy_pixels(
-            interleaved_view(reader._info._width, 1, (image_t::view_t::x_iterator) buffer, reader._scanline_length)
+            interleaved_view(reader._info._width, 1,
+                (image_t::view_t::x_iterator) buffer, reader._scanline_length)
             , subimage_view(view(dst), 0, row, reader._info._width, 1));
 
         ++row;
