d8cc5325005554260524db99240370e5d5a5f456
Mateusz Loskot <mateusz@loskot.net>
2018-03-25 22:19:24 +0200

    Dump indexed image to hex

--- indexed_image_raw.cpp
+++ indexed_image_raw.cpp
@@ -2,57 +2,60 @@
 #include <cstdint>
 #include <iostream>
 #include <vector>
+#include <boost/algorithm/hex.hpp>
 #include <boost/gil/gil_all.hpp>
 #include <boost/gil/extension/toolbox/image_types/indexed_image.hpp>
-
 using namespace std;
 using namespace boost::gil;
+
+template <typename View>
+void dump_view(std::ostream& os, View const& view)
+{
+    using pixel_t = pixel<typename channel_type<View>::type, layout<typename color_space_type<View>::type>>;
+    std::vector<pixel_t> row_buf(view.width());
+    std::string row_hex(2 * row_buf.size() * sizeof(pixel_t), char{0});
+    for (std::ptrdiff_t y = 0; y < view.height(); ++y)
+    {
+        std::copy(view.row_begin(y), view.row_end(y), row_buf.begin());
+
+        auto beg = reinterpret_cast<unsigned char*>(row_buf.data());
+        auto end = beg + row_buf.size() * sizeof(pixel_t);
+
+        boost::algorithm::hex(beg, end, row_hex.begin());
+        os << row_hex << '\n';
+    }
+}
 
 int main()
 {
     try
     {
-        vector<uint8_t> indices(2 * 2);
-        vector<rgb8_pixel_t> palette(2);
+        rgb8_pixel_t red8(255, 0, 0), green8(0, 255, 0), blue8(0, 0, 255), white8(255, 255, 255);
 
-        auto indices_view = interleaved_view(2, 2
-            , (gray8_image_t::view_t::x_iterator) indices.data()
-            , 2 // row size in bytes
-        );
-        auto palette_view = interleaved_view(2, 1
-            , (rgb8_image_t::view_t::x_iterator) palette.data()
-            , 2 * 3 // row size in bytes
-        );
+        int w = 4, h =4;
+        vector<uint8_t> indices(w * h, 0); // fill white
+        vector<rgb8_pixel_t> palette{white8, blue8};
 
-        auto i = indices_view.begin();
-        *i = 0; i++;
-        *i = 1; i++;
-        *i = 0; i++;
-        *i = 1; i++;
-        assert(i == indices_view.end());
+        auto indices_view = interleaved_view(w, h, (gray8_image_t::view_t::x_iterator) indices.data(), w);
+        auto loc = indices_view.xy_at(0, indices_view.height() - 1);
+        for (int y = 0; y < indices_view.height(); ++y)
+        {
+            *loc = 1;
+            ++loc.x();
+            loc.y()--;
+        }
 
-        auto c = palette_view.begin();
-        *c =  rgb8_pixel_t(255, 0, 0); c++;
-        *c =  rgb8_pixel_t(0, 0, 255); c++;
-        assert(c == palette_view.end());
+        auto palette_view = interleaved_view(2, 1, (rgb8_image_t::view_t::x_iterator) palette.data(), 2 * 3);
+        auto ii_view = view(indices_view, palette_view);
 
-        using image_t = indexed_image_deref_fn<gray8_image_t::const_view_t::xy_locator, rgb8_image_t::const_view_t::xy_locator>;
-        image_t ii(indices_view.xy_at(0, 0), palette_view.xy_at(0, 0));
-        auto p00 = ii(point_t(0, 0));
-        assert(p00[0] == 255 && p00[1] == 0 && p00[2] == 0);
-        auto p10 = ii(point_t(1, 0));
-        assert(p10[0] == 0 && p10[1] == 0 && p10[2] == 255);
-        auto p01 = ii(point_t(0, 1));
-        assert(p01[0] == 255 && p01[1] == 0 && p01[2] == 0);
-        auto p11 = ii(point_t(1, 1));
-        assert(p11[0] == 0 && p11[1] == 0 && p11[2] == 255);
+        rgb8_image_t img(ii_view.dimensions());
+        auto img_view = view(img);
+        fill_pixels(img_view, white8);
 
-        using locator_t = indexed_image_locator_type<gray8_image_t::const_view_t::xy_locator, rgb8_image_t::const_view_t::xy_locator>::type;
-        using indexed_image_view_t = indexed_image_view<locator_t>;
-        locator_t locator{point_t{0, 0}, point_t{1, 1}, ii};
-        indexed_image_view_t ii_view{{2, 2}, 2, locator};
+        copy_pixels(ii_view, view(img));
+        dump_view(std::cout, view(img));
 
-
+        return EXIT_SUCCESS;
     }
     catch (std::exception const& e)
     {
